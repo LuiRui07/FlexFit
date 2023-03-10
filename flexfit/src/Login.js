f3b95e1c57d52ef3675a702a63fe166696a50e8d
Luis Ruiz <100932068+LuiRui07@users.noreply.github.com>
2023-03-10 15:56:44 +0100

    Hola

---
+++ flexfit/src/Login.js
@@ -0,0 +1,19 @@
+import "./css/Login.css";
+import App from "./App.js";
+
+export default function LoginPage() {
+  return (
+    <div className="LoginPage">
+      <h1 className="LoginPageTitle">Log in to FlexFit</h1>
+      <form>
+        <input type="text" name="username"></input>
+        <br></br>
+        <input type="password" name="password" />
+        <br></br>
+        <button type="submit" variant="primary" onClick={App}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
