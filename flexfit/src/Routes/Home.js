2dea0b89ac89aba181d4fac82c8456298acf5295
Luis Ruiz <100932068+LuiRui07@users.noreply.github.com>
2023-03-22 22:56:49 +0100

    Mejora Barra Navegacion y más ejercicios

--- flexfit/src/Routes/Home.js
+++ flexfit/src/Routes/Home.js
@@ -1,13 +1,13 @@
 import HeaderFF from "./HeaderFF";
-import '../css/Home.css'
+import "../css/Home.css";
 
 export default function Home() {
   return (
     <div className="h-100">
-      <HeaderFF/>
+      <HeaderFF />
       <div className="fuerte">
         <h1>
-          Hola @bellaco<br></br>
+          Hola mi leal @usuario<br></br>
           Ready to do exercise?
         </h1>
       </div>
