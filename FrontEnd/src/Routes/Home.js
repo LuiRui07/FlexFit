6fdb1d56d06f0bdb6f9303f48eab13361e9f0ec6
Luis Ruiz <100932068+LuiRui07@users.noreply.github.com>
2023-04-05 16:56:16 +0200

    Barra de busqueda

--- FrontEnd/src/Routes/Home.js
+++ FrontEnd/src/Routes/Home.js
@@ -1,21 +1,19 @@
 import HeaderFF from "./HeaderFF";
-import '../css/Home.css'
+import "../css/Home.css";
 
-import axios from 'axios';
+import axios from "axios";
 
 export default function Home() {
-
-  axios.get('http://localhost:7777/api/exercise').then((res) => { 
+  axios.get("http://localhost:7777/api/exercise").then((res) => {
     console.log(res.data);
   });
-  
 
   return (
     <div className="h-100">
-      <HeaderFF/>
+      <HeaderFF />
       <div className="fuerte">
         <h1>
-          Hola @bellaco<br></br>
+          Hola @username<br></br>
           Ready to do exercise?
         </h1>
       </div>
