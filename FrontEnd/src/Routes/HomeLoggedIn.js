8fe4aac3c9e0935f63101a6b24ffcdf2e5e626be
Alba <100930901+albadelatorres@users.noreply.github.com>
2023-04-14 15:52:04 +0200

    Update HomeLoggedIn.js

--- FrontEnd/src/Routes/HomeLoggedIn.js
+++ FrontEnd/src/Routes/HomeLoggedIn.js
@@ -1,31 +1,80 @@
-import React from 'react'
-import HeaderFF from "./HeaderFF";
-import { useEffect, useState } from 'react';
-
-
+import foto1 from "../images/pt.png";
+import foto2 from "../images/avanzado.png"
+import foto3 from "../images/historial.png"
+import React, { useEffect, useState } from 'react';
+import HeaderFF from './HeaderFF';
+import '../css/HomeLoggedIn.css';
 
 function HomeLoggedIn() {
-
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    document.getElementById("homeHeaderLi").classList.add("active");
+    document.getElementById('homeHeaderLi').classList.add('active');
+    const bootstrapCss = document.createElement('link');
+    const bootstrapJs = document.createElement('script');
+
+    bootstrapCss.href = 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css';
+    bootstrapCss.rel = 'stylesheet';
+    bootstrapCss.type = 'text/css';
+
+    bootstrapJs.src = 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/js/bootstrap.bundle.min.js';
+    bootstrapJs.type = 'text/javascript';
+    bootstrapJs.async = true;
+
+    document.head.appendChild(bootstrapCss);
+    document.body.appendChild(bootstrapJs);
 
     localStorage.getItem('user') && setUser(JSON.parse(localStorage.getItem('user')));
-  },[]);
-
+  }, []);
 
   return (
     <div>
-        <HeaderFF />  
-        <div className="fuerte">
-        <h1>
+      <HeaderFF />
+
+      <div className="fuerte">
+        <div className="p-4">
+          <h1>
             Hola {JSON.stringify(user)}<br></br>
             Ready to do exercise?
-        </h1>
+          </h1>
         </div>
+        <div className="row p-4">
+          <div className="col-sm">
+            <div className="card h-100">
+              <div className="card-body text-dark">
+                <h5 className="card-title">Principiantes</h5>
+                <img src={foto1} class="img-fluid" alt="image not found"/>
+                <p className="card-text pt-3" style={{ fontSize: '19px' }}>Para usuarios sin previa experiencia, tenemos un catálogo de rutinas prediseñadas.</p>
+                <a href="/Workouts" className="btn btn-primary">Catálogo</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm">
+            <div className="card h-100">
+              <div className="card-body text-dark">
+                <h5 className="card-title">Avanzado</h5>
+                <img src={foto2} class="img-fluid" alt="image not found"/>
+                <p className="card-text pt-3" style={{ fontSize: '19px' }}>Diseñe su propia rutina, para usuarios con más experiencia.</p>
+                <a href="/Workouts" className="btn btn-primary">Diseñar rutina</a>
+              </div>
+            </div>
+          </div>
+          {/*<div className="col-sm">
+            <div className="card h-100">
+              <div className="card-body text-dark">
+                <h5 className="card-title">Historial</h5>
+                <img src={foto3} class="img-fluid" alt="image not found"/>
+                <p className="card-text pt-3 pb-4" style={{ fontSize: '19px' }}>Consulte antiguas rutinas realizadas.</p>
+                <a href="/Historial" className="btn btn-primary">Historial</a>
+              </div>
+            </div>
+  </div>*/}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomeLoggedIn
+export default HomeLoggedIn;
+
+
