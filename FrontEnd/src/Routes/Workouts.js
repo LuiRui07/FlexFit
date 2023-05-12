1ecd3ef4fdd17a5c41aa64a9b044430e6bc57bae
rafacm1803 <rafacebamar@gmail.com>
2023-05-12 19:55:37 +0200

    eliminar rutinas en card

--- FrontEnd/src/Routes/Workouts.js
+++ FrontEnd/src/Routes/Workouts.js
@@ -44,6 +44,38 @@ export default function AllWorkouts() {
     e.preventDefault();
     window.location.href = "/rutina/" + id;
   };
+
+ 
+
+  const [showModal, setShowModal] = useState(false);
+
+ 
+
+  const handleDeleteClick = (event) => {
+    event.stopPropagation(); // Evita que el evento se propague al card
+    setShowModal(true);
+  };
+
+  const handleModalClose = (event) => {
+    event.stopPropagation();
+    setShowModal(false);
+  };
+
+
+  function borrarRutina(workout,event) {
+    event.stopPropagation();
+    console.log("Eliminando rutina con id: " + workout.id);
+    setShowModal(false);
+    axios
+      .delete("http://localhost:7777/api/workout/" + workout.id, { id: workout.id })
+      .then((response) => {
+        console.log(response.data);
+        alert("Borrado");
+        setShowModal(false);
+        window.location.href = "/workouts";
+      });
+  }
+
   document.title="Rutinas";
   return (
     <div className="backgroudTochoWorkout">
@@ -52,7 +84,7 @@ export default function AllWorkouts() {
         <ul className="breadcrumb">
           <li><a href="/">FlexFit</a></li>
           <li>/</li>
-          <li className="breadcrumbActive">Mis Rutinas</li>  
+          <li className="breadcrumbActive">Mis Rutinas</li>
         </ul>
 
         <h1>Rutinas</h1>
@@ -85,13 +117,46 @@ export default function AllWorkouts() {
               onClick={goToRoutine(workout.id)}
             >
               {workout.private === true && (
-                <h5 className="card-title titleWorkout ">{workout.name}</h5>
+                <div className="card-title titleWorkout">
+                  <div className="borrarRutina" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <h5>{workout.name}</h5>
+                    <div>
+                      <button type="button" name="preAlert" className="btn btn-danger custom-cursor-on-hover" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={handleDeleteClick} onMouseDown={(event) => event.stopPropagation()}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3-fill custom-cursor-on-hover" viewBox="0 0 16 16">
+                          <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" className="custom-cursor-on-hover"></path>
+                        </svg>
+                      </button>
+                      {showModal === true && (
+                      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <h1 class="modal-title fs-5" id="exampleModalLabel">Borrar rutina</h1>
+                              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                              ¿Estás seguro? Esta accion no sera reversible.
+                            </div>
+                            <div class="modal-footer">
+                              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" onClick={handleModalClose}>Cancelar</button>
+                              <button type="button" onClick={(event) => borrarRutina(event)} class="btn btn-danger" >Confirmar</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      )}
+                    </div>
+                  </div>
+                </div>
+
+
               )}
               {workout.private === false && (
                 <h5 className="card-title titleWorkout publicWorkout">
                   {workout.name}
                 </h5>
               )}
+
 
               <div className="card-body">
                 <p className="card-text">
@@ -104,6 +169,7 @@ export default function AllWorkouts() {
                   Dificultad: <strong>{workout.difficulty}</strong>
                 </p>
               </div>
+
             </div>
           ))}
         </div>
